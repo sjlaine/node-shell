1de55a38b77e670e01a588a994c5372e28f9ff3d
Sarah Laine <sjlaine624@gmail.com>
2018-01-15 14:05:11 -0500

    bash.js up to head and cat

---
+++ bash.js
@@ -0,0 +1,27 @@
+// // Output a prompt
+process.stdout.write('prompt > ');
+
+// The stdin 'data' event fires after a user types in a line
+process.stdin.on('data', function (data) {
+  var trimmed = data.toString().trim(); // remove the newline
+  var cmd = '';
+  for (var i = 0; i < trimmed.length; i++) {
+    if (trimmed[i] === ' ') {
+      var arg = trimmed.slice(i + 1);
+      break;
+    }
+    cmd += trimmed[i];
+  }
+  var commands = require('./commands.js');
+  // commands[cmd]();
+
+  // process.stdout.write('You typed: ' + cmd);
+
+    commands[cmd](arg);
+  // process.stdout.write('\nprompt > ');
+});
+
+
+// console.log(process.mainModule.paths[0]);
+
+// console.log(Date());
---
+++ commands.js
@@ -0,0 +1,65 @@
+var exports = module.exports = {};
+const fs = require('fs');
+
+exports.pwd = function() {
+    process.stdout.write(process.mainModule.paths[0]);
+    process.stdout.write('\nprompt > ');
+}
+
+exports.date = function() {
+    process.stdout.write(Date());
+    process.stdout.write('\nprompt > ');
+}
+
+exports.ls = function() {
+  fs.readdir('.', function(err, files) {
+    if (err) throw err;
+
+    files.forEach(function() {
+      process.stdout.write(file.toString() + "\n");
+    });
+    process.stdout.write('\nprompt > ');
+})}
+
+exports.echo = function(arg) {
+  process.stdout.write(arg + "\n");
+  process.stdout.write('\nprompt > ');
+}
+
+exports.cat = function(file) {
+  const rr = fs.createReadStream(file);
+  rr.on('readable', () => {
+    process.stdout.write(`readable: ${rr.read()}`);
+  });
+  rr.on('end', () => {
+    process.stdout.write('\nend');
+  });
+
+  process.stdout.write('\nprompt > ');
+}
+
+exports.head = function(file) {
+  // const rr = fs.createReadStream(file);
+    fs.readFile(file.toString(), function (err, data) {
+      if (err) throw err;
+
+      var lines = data.toString().split('\n');
+
+      for (var i = 0; i < 5; i++) {
+          process.stdout.write(lines[i] + '\n');
+      }
+    });
+  // rr.on('readable', () => {
+  //   process.stdout.write(`readable: ${rr.pause()}`);
+  // });
+  // rr.on('readable', () => {
+  //   process.stdout.write(`readable: ${rr.read()}`);
+  // });
+  process.stdout.write('\nprompt > ');
+
+}
+
+exports.tail = function(file) {
+
+}
+
